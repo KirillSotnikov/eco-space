2591fc57fc00e2a79382ae7f80208a5be27740a7
Kirill <sotnikov_k@outlook.com>
2019-12-22 01:52:44 +0300

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+
+<template>
+  <!-- App.vue -->
+
+  <v-app>
+    <v-navigation-drawer v-model="isDrawer" app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <!-- -->
+       <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Eco Space</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <nuxt/>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isDrawer: false
+    }
+  },
+  methods: {
+    toggleDrawer () {
+      this.isDrawer = !this.isDrawer
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div>
+    <app-slider />
+    <app-card-list />
+  </div>
+</template>
+
+<script>
+
+import Slider from '@/components/MainPage/Slider'
+import CardList from '@/components/MainPage/CardList'
+
+export default {
+  components: {
+    'app-slider': Slider,
+    'app-card-list': CardList
+  },
+  data () {
+    return {
+      testData: 'Text'
+    }
+  }
+}
+
+</script>

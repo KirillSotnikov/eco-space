b959e245a67d81e3571ea15e0e163983acc25093
kirill <kirill@Mac-mini-kirill.local>
2020-01-09 17:11:43 +0200

    add edit course page

---
+++ components/coursePage/Form/coursesForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row>
+    <v-col cols="12" md="6">
+      <p>Starting course</p>
+      <v-date-picker full-width landscape color="blue" :min="minDate" v-model="startCourseDate"></v-date-picker>
+    </v-col>
+    <v-col cols="12" md="6">
+      <v-textarea
+        outlined
+        name="input-7-4"
+        label="Course description"
+        v-model="courseDescription"
+      ></v-textarea>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: [
+    'startCourseDate',
+    'courseDescription'
+  ],
+  data () {
+    return {
+      minDate: new Date().toISOString().substr(0, 10)
+    }
+  }
+}
+</script>
---
+++ pages/courses/edit/_name.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container fluid>
+    <h1 class="mb-7">Edit - {{courseName}}</h1>
+    <app-courses-form :startCourseDate="startCourseDate" :courseDescription="courseDescription" />
+  </v-container>
+</template>
+
+<script>
+import courseForm from '@/components/coursePage/Form/coursesForm.vue'
+export default {
+  asyncData ({ params }) {
+    return {
+      courseName: params.name
+    }
+  },
+  data () {
+    return {
+      startCourseDate: new Date().toISOString().substr(0, 10),
+      courseDescription: ''
+    }
+  },
+  components: {
+    'app-courses-form': courseForm
+  }
+}
+</script>
